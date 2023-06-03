f0451646083d85fa81a555b67c75b2e9ac6db6e4
joaoleandro <new.joaoleandro@gmail.com>
2023-06-02 22:51:53 -0300

    desenvolvendo a logica para buscar os pokemons na API

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,42 @@
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=10`;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=150`;
 fetch(url)
 .then((response) => response.json())
-.then((bodyResponse) => bodyResponse.results.map((name) => {
-    console.log(name.name)
-    createCardPokemon()
+.then((bodyResponse) => bodyResponse.results.map((item) => {
+    fetch(item.url)
+    .then((response) => response.json())
+    .then((teste) => {
+        let num = 0
+        const x = teste.types
+        console.log(x[num].type.name)
+
+        const createCardPokemon = () => {
+            const sectionCard = document.querySelector(".section-cards-pokemon")
+            sectionCard.innerHTML += `
+                <div class="card-pokemon">
+                    <div class="informations-pokemon">
+                    <h3 class="name-pokemon">${item.name}</h3>
+                    <span># ${teste.id}</span>
+                    </div>
+                    <img src="${teste.sprites.front_default}" alt="${item.name}" class="image-pokemon">
+                    <div class="type-pokemon">
+                        <span>${x[num].type.name}</span>
+                        <span></span>
+                    </div>
+                </div>
+            `
+            num++
+            return sectionCard
+        }
+        createCardPokemon()
+    })
+    
+    
+
 }) )
 .catch((error) => console.error(error))
 
 
-const createCardPokemon = () => {
-    const sectionCard = document.querySelector(".section-cards-pokemon")
-    sectionCard.innerHTML += `
-        <div class="card-pokemon">
-            <div class="informations-pokemon">
-                <h3 class="name-pokemon">Bulbasaur</h3>
-                <span># 1</span>
-            </div>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" class="image-pokemon">
-            <div class="type-pokemon">
-                <span>Grass</span>
-                <span>Overgrow</span>
-            </div>
-        </div>
-    `
-    return sectionCard
-}
 
 
 
