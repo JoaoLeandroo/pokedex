201652c011d0d9149d7eecf712ef9ea86fe26dc7
joaoleandro <new.joaoleandro@gmail.com>
2023-06-03 08:37:19 -0300

    adicionado todos os 151 pokemons da primeira geração

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,42 +1,40 @@
+const url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=151`;
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=150`;
-fetch(url)
-.then((response) => response.json())
-.then((bodyResponse) => bodyResponse.results.map((item) => {
-    fetch(item.url)
-    .then((response) => response.json())
-    .then((teste) => {
-        let num = 0
-        const x = teste.types
-        console.log(x[num].type.name)
+const createCardPokemon = (item, teste) => {
+  let num = 0;
+  const x = teste.types;
 
-        const createCardPokemon = () => {
-            const sectionCard = document.querySelector(".section-cards-pokemon")
-            sectionCard.innerHTML += `
-                <div class="card-pokemon">
-                    <div class="informations-pokemon">
-                    <h3 class="name-pokemon">${item.name}</h3>
-                    <span># ${teste.id}</span>
-                    </div>
-                    <img src="${teste.sprites.front_default}" alt="${item.name}" class="image-pokemon">
-                    <div class="type-pokemon">
-                        <span>${x[num].type.name}</span>
-                        <span></span>
-                    </div>
-                </div>
-            `
-            num++
-            return sectionCard
-        }
-        createCardPokemon()
-    })
-    
-    
+  const sectionCard = document.querySelector(".section-cards-pokemon");
+  sectionCard.innerHTML += `
+    <div class="card-pokemon">
+        <div class="informations-pokemon">
+        <h3 class="name-pokemon">${item.name}</h3>
+        <span># ${teste.id}</span>
+        </div>
+        <img src="${teste.sprites.front_default}" alt="${item.name}" class="image-pokemon">
+        <div class="type-pokemon">
+            <span>type - ${x[num].type.name}</span>
+        </div>
+    </div>
+  `;
+  num++;
+  return sectionCard;
+};
 
-}) )
-.catch((error) => console.error(error))
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const bodyResponse = await response.json();
 
+    for (const item of bodyResponse.results) {
+      const pokemonResponse = await fetch(item.url);
+      const teste = await pokemonResponse.json();
+      createCardPokemon(item, teste);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-
-
+fetchData(url);
 
